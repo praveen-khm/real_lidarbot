f618af9254ad612c182cb3d23a8d3d5ced02a63b
TheNoobInventor <eduobijoro@mailfence.com>
2023-02-04 07:05:55 +0000

    Changes to lidarbot_launch.py

--- launch/lidarbot_launch.py
+++ launch/lidarbot_launch.py
@@ -62,7 +62,7 @@ def generate_launch_description():
     
     # Publish the joint state values for the non-fixed joints in the URDF file.
     start_joint_state_publisher_cmd = Node(
-        condition=UnlessCondition(gui),
+        # condition=UnlessCondition(gui),
         package='joint_state_publisher',
         executable='joint_state_publisher',
         name='joint_state_publisher')
