6f43d58d9e0b209695a65f6a15898d4d1b058741
TheNoobInventor <eduobijoro@mailfence.com>
2023-02-07 16:44:53 +0000

    Commented out joint state publishere command in sim launch file

--- launch/lidarbot_sim_launch.py
+++ launch/lidarbot_sim_launch.py
@@ -123,7 +123,7 @@ def generate_launch_description():
     ld.add_action(declare_world_cmd)
     
     # Add any actions
-    ld.add_action(start_joint_state_publisher_cmd)
+    # ld.add_action(start_joint_state_publisher_cmd)
     ld.add_action(start_gazebo_server_cmd)
     ld.add_action(start_gazebo_client_cmd)
     ld.add_action(start_robot_state_publisher_cmd)
