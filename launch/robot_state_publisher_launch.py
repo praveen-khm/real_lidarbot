8f1004ed501732fd116929c2bea374c38bba701d
TheNoobInventor <eduobijoro@mailfence.com>
2023-02-09 09:32:34 +0000

    Minor correction in robot state publisher launch file

--- launch/robot_state_publisher_launch.py
+++ launch/robot_state_publisher_launch.py
@@ -3,7 +3,7 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.substitution import LaunchConfiguration, Command
+from launch.substitutions import LaunchConfiguration, Command
 from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
 
